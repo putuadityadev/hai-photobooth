e1df409a6c7f419f86f47e1e41c8c9b1e28c201c
aditya <adityamph1@gmail.com>
2025-01-29 17:46:32 +0800

    fix 1

--- data/maindata.js
+++ data/maindata.js
@@ -1,17 +1,17 @@
 export const cardImageData = [
   {
     id: 1,
-    image: "public/images/image1.png",
+    image: "images/image1.png",
     isActive : false
   },
   {
     id: 2,
-    image: "public/images/image2.png",
+    image: "images/image2.png",
     isActive: true,
   },
   {
     id: 3,
-    image: "public/images/image3.png",
+    image: "images/image3.png",
     isActive: false
   }
 ];
@@ -21,19 +21,19 @@ export const cardItems = [
     id: 1,
     title: "Kostumisasi Tema Sesuai Acara Kamu",
     desc: "Kami menawarkan berbagai tema yang dapat disesuaikan.",
-    image: "public/images/Confetti.png"
+    image: "images/Confetti.png"
   },
   {
     id: 2,
     title: "Kualitas Foto Tinggi dan Instan",
     desc: "Nikmati hasil foto berkualitas tinggi dalam sekejap.",
-    image: "public/images/Photo Gallery.png"
+    image: "images/Photo Gallery.png"
   },
   {
     id: 3,
     title: "Aksesoris Seru untuk Setiap Momen",
     desc: "Pilih dari berbagai aksesori menarik untuk foto Anda.",
-    image: "public/images/Sun Glasses.png"
+    image: "images/Sun Glasses.png"
   }
 ];
 
@@ -42,44 +42,44 @@ export const testiData = [
     name: 'Dewi Sintya',
     title: 'Event Organizer, Bali',
     testimonial: 'Foto-foto yang diberikan sangat bagus, dan berkualitas tinggi',
-    stars: 'public/images/stars.png',
-    photo: 'public/images/profile-1.jpg'
+    stars: 'images/stars.png',
+    photo: 'images/profile-1.jpg'
   },
   {
     name: 'Moe',
     title: 'Pengantin, Bali',
     testimonial: 'Kualitas pelayanan sangat baik, dan bisa disesuaikan dengan tema acara saya',
-    stars: 'public/images/stars.png',
-    photo: 'public/images/profile-3.jpg'
+    stars: 'images/stars.png',
+    photo: 'images/profile-3.jpg'
   },
   {
     name: 'Kadek Agung',
     title: 'Manajer, Perusahan ABC',
     testimonial: 'Keren, photobooth yang bisa disesuakan dan fleksibel sangatlah bagus bagi saya',
-    stars: 'public/images/stars.png',
-    photo: 'public/images/profile-2.jpg'
+    stars: 'images/stars.png',
+    photo: 'images/profile-2.jpg'
   }
 ];
 
 export const linksSocialMedia = [
   {
     name : 'Facebook',
-    image : 'public/images/Facebook.png'
+    image : 'images/Facebook.png'
   },
   {
     name : 'Instagram',
-    image : 'public/images/Instagram.png'
+    image : 'images/Instagram.png'
   },
   {
     name : 'X',
-    image : 'public/images/X.png'
+    image : 'images/X.png'
   },
   {
     name : 'LinkedIn',
-    image : 'public/images/LinkedIn.png'
+    image : 'images/LinkedIn.png'
   },
   {
     name : 'YouTube',
-    image : 'public/images/Youtube.png'
+    image : 'images/Youtube.png'
   }
 ]
